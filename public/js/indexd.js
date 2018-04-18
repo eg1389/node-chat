3f82c144295af3bbe43b502750e5860cbc04833a
حمید شجاعی <h-shojaee@Artman-co.com>
2018-04-18 14:38:10 +0430

    first commit eight

--- public/js/indexd.js
+++ public/js/indexd.js
@@ -24,12 +24,20 @@ var socket = io();
 
    // for broadcasting
    socket.on('newMessB',function(eee){
-    var formattedTime = moment(eee.createat).locale('fa').format("jYYYY/jMM/jD - h:mm a");
+       var formattedTime = moment(eee.createat).locale('fa').format("jYYYY/jMM/jD - h:mm a");
+       var templateNM = jQuery('#message-template').html();
+       var html = Mustache.render(templateNM,{
+           text:eee.text,
+           from : eee.from,
+           createat : formattedTime
+       });
+       jQuery('#messol').append(html);
+    //var formattedTime = moment(eee.createat).locale('fa').format("jYYYY/jMM/jD - h:mm a");
      //console.log('brodcast',eee);
-     var li=jQuery('<li></li>');
+     //var li=jQuery('<li></li>');
     
-     li.text(`${eee.from} ${formattedTime} : ${eee.text}`);
-     jQuery('#messol').append(li);
+     //li.text(`${eee.from} ${formattedTime} : ${eee.text}`);
+     //jQuery('#messol').append(li);
    });
 
    socket.on('payamO',function(one){
@@ -73,13 +81,24 @@ locationButton.removeAttr('disabled').text('send location');
 
 
 socket.on('newLocation',function(locaO){
-    var loctime = moment(locaO.createat).format("jYYYY/jMM/jD");
+ var loctime = moment(locaO.createat).locale('fa').format("jYYYY/jMM/jD");
+ var templateLO = jQuery('#location-template').html();
+ var htmlLO = Mustache.render(templateLO,{
+     from:locaO.from,
+     url:locaO.url,
+     createat:loctime
+ });
+ jQuery('#messol').append(htmlLO);
+ 
+
+ 
+    /*var loctime = moment(locaO.createat).format("jYYYY/jMM/jD");
 var li = jQuery('<li></li>');
 var a = jQuery('<a target="_blank">My Location</a>')
 li.text(`${loctime} -- ${locaO.from} : `);
 a.attr('href',`${locaO.url}`);
 li.append(a);
-jQuery('#messol').append(li);
+jQuery('#messol').append(li);*/
 });
 
     socket.on('disconnect',function (){
